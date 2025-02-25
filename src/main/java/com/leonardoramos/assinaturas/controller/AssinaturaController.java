2c14e0cfb747aa3739d56fc2f18dfb12d93715af
LE0N4RDOR4M0S <leomatisa1000@gmail.com>
2025-02-25 16:37:09 -0400

    feat: Adição de controlador Assinatura

---
+++ src/main/java/com/leonardoramos/assinaturas/controller/AssinaturaController.java
@@ -0,0 +1,43 @@
+package com.leonardoramos.assinaturas.controller;
+
+import com.leonardoramos.assinaturas.model.Assinatura;
+import com.leonardoramos.assinaturas.service.AssinaturaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/assinaturas")
+public class AssinaturaController {
+
+    private final AssinaturaService assinaturaService;
+
+    public AssinaturaController(AssinaturaService assinaturaService) {
+        this.assinaturaService = assinaturaService;
+    }
+
+    @GetMapping
+    public List<Assinatura> buscarTodas() {
+        return assinaturaService.buscarTodas();
+    }
+
+    @GetMapping("/{id}")
+    public Assinatura buscarPorId(@PathVariable(name = "id") String id){
+        return assinaturaService.buscarPorId(id);
+    }
+
+    @PostMapping
+    public Assinatura criar(Assinatura assinatura){
+        return assinaturaService.criar(assinatura);
+    }
+
+    @PostMapping("/{id}/atualizar")
+    public Assinatura atualizar(@PathVariable(name = "id") String id, Assinatura novaAssinatura) {
+        return assinaturaService.atualizar(id, novaAssinatura);
+    }
+
+    @GetMapping("/{id}/deletar")
+    public void deletar(@PathVariable(name = "id") String id){
+        assinaturaService.deletar(id);
+    }
+}
