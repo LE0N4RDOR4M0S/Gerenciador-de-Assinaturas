a3bade03d830f14373db74a5a62d3d5c005cbb19
LE0N4RDOR4M0S <leomatisa1000@gmail.com>
2025-03-13 20:40:29 -0400

    feat: Criação de DTOs para Cupom

--- src/main/java/com/leonardoramos/assinaturas/model/Cupom.java
+++ src/main/java/com/leonardoramos/assinaturas/model/Cupom.java
@@ -2,10 +2,7 @@ package com.leonardoramos.assinaturas.model;
 
 import com.leonardoramos.assinaturas.auditoria.AuditoriaListener;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -15,6 +12,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @EntityListeners(AuditoriaListener.class)
 public class Cupom {
     @Id
