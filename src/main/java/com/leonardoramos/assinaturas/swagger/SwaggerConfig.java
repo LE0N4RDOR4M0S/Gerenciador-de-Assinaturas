401a1649c54f26328d9bf448c0545879475408b1
LE0N4RDOR4M0S <leomatisa1000@gmail.com>
2025-04-11 19:45:53 -0400

    feat: Authorize no Swagger

--- src/main/java/com/leonardoramos/assinaturas/swagger/SwaggerConfig.java
+++ src/main/java/com/leonardoramos/assinaturas/swagger/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.leonardoramos.assinaturas.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,11 +18,19 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("API de gerenciamento de Assinaturas")
-                        .version("0.6.0")
+                        .version("0.8.0")
                         .description("Documentação da API do Gerenciamento de Assinaturas virtuais")
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")))
                 .externalDocs(new ExternalDocumentation()
                         .description("Mais informações")
-                        .url("https://github.com/seu-repositorio"));
+                        .url("https://github.com/seu-repositorio"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT Token")));
     }
 }
